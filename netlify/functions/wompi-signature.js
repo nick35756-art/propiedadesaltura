a821f9b2c6ae8666fda67984587b879733eed067
Nick <nick35756@gmail.com>
2025-11-28 00:00:40 -0500

    Create wompi-signature.js

---
+++ netlify/functions/wompi-signature.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
+
+  const { reference, amountInCents, currency } = JSON.parse(event.body);
+  const integritySecret = 'YOUR_INTEGRITY_SECRET_HERE';  // From Wompi Dashboard > Settings > Developers > Integrity Secret
+
+  // Generate signature: SHA256(reference + amountInCents + currency + secret)
+  const concat = `${reference}${amountInCents}${currency}${integritySecret}`;
+  const signature = crypto.createHmac('sha256', integritySecret).update(concat).digest('hex');
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ signature })
+  };
+};

c49c2a28f61cb6f0196837eb2746da0b569703b1
Nick <nick35756@gmail.com>
2025-11-28 04:39:39 -0500

    Update wompi-signature.js

--- netlify/functions/wompi-signature.js
+++ netlify/functions/wompi-signature.js
@@ -1,12 +1,14 @@
 const crypto = require('crypto');
 
 exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Method Not Allowed' };
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
 
   const { reference, amountInCents, currency } = JSON.parse(event.body);
   const integritySecret = 'test_integrity_MwApPTzFs1oiqhDasUSYl1VI1EkpEmmj';  // Your sandbox secret
 
-  // Docs concat: reference + amountInCents + currency + integritySecret (NO separators!)
+  // Docs concat: direct + (no separators!)
   const concatString = reference + amountInCents + currency + integritySecret;
   const signature = crypto.createHmac('sha256', integritySecret).update(concatString).digest('hex');
 
